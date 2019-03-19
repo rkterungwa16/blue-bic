1e35b2db4e30efcae0b87675bf8bdb6ec054b7e4
rkterungwa16 <terungwakombol@gmail.com>
2019-03-19 08:27:12 +0100

    Implement read books page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import Header from './components/Header'
 import Welcome from './components/Welcome'
 import Review from './components/Review'
+import Read from './components/Read'
 import './styles/styles.scss'
 import 'circular-std';
 import './assets/logo.png'
@@ -16,6 +17,7 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={Welcome} />
           <Route exact path='/review-books' component={Review} />
+          <Route exact path='/read-books' component={Read} />
         </Switch>
       </div>
     )
--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 
-import formCircle from '../assets/review-middle-ellipse.svg'
-import lightBulb from '../assets/review-star.svg'
+import formCircle from '../assets/read-green-ellipse.svg'
+import lightBulb from '../assets/read-book.svg'
 import rightArrow from '../assets/review-arrow.svg'
 import lowerCircle from '../assets/review-lower-ellipse.svg'
-import reviewRedCircle from '../assets/review-red-ellipse.svg'
-import reviewBlueCirce from '../assets/review-blue-ellipse.svg'
+import readRedCircle from '../assets/read-red-ellipse.svg'
+import readBlueCirce from '../assets/read-blue-ellipse.svg'
 
-const Review = () => (
+const Read= () => (
   <div className='welcome__container'>
     <div className='welcome-form__wrapper'>
-      <img src={reviewRedCircle} alt='Red Circle' className='review-red-circle' />
-      <img src={reviewBlueCirce} alt='Blue Circle' className='review-blue-circle' />
+      <img src={readRedCircle} alt='Red Circle' className='red-circle' />
+      <img src={readBlueCirce} alt='Blue Circle' className='blue-circle' />
       <div className='form-bulb__wrapper'>
         <div className='review-image__wrapper'>
           <img src={lightBulb} alt='Light Bulb' className='review__star' />
           <img src={formCircle} alt='Form Circle' className='review-mid__circle' />
         </div>
         <div className='review-text__wrapper'>
-          <span className='review__text'>Review Books</span>
+          <span className='review__text'>Read Books</span>
           <span className='review-msg__text'>Create your account to get started. After that, you can share books and make friends.</span>
         </div>
         <div className='dot__wrapper'>
@@ -36,4 +36,4 @@ const Review = () => (
   </div>
 )
 
-export default Review
+export default Read
