aa98b1d3975a36ed44660aaff711dc7ae1749b9e
rkterungwa16 <terungwakombol@gmail.com>
2019-03-19 09:31:44 +0100

    modify welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -10,19 +10,26 @@ const Welcome = () => (
     <div className='welcome-form__wrapper'>
       <img src={topCornerCircle} alt='Top Corner Circle' className='form-circle__top' />
       <div className='form-bulb__wrapper'>
-        <img src={lightBulb} alt='Light Bulb' className='form-bulb__img' />
-        <img src={formCircle} alt='Form Circle' />
-        <span className='welcome__text'>Welcome!</span>
-        <span className='welcome-msg__text'>Create your account to get started. After that, you can share books and make friends.</span>
-        <span className='welcome-email__text'>Email</span>
-        <img src={inputCircle} alt='Input Circle' className='input-circle' />
-        <input className='welcome-email__input' type='email' />
-        <span className='welcome-password__text'>Password</span>
-        <input className='welcome-password__input' type='password' />
-        <span className='welcome-confirm-password__text'>Type Password Again</span>
-        <input className='welcome-confirm-password__input' type='password' />
-        <button className='welcome-btn__signup'>Signup</button>
-        <span className='redirect-login__text'>Already have an account? <a href='#/login'>Login</a></span>
+        <div className='welcome-image__wrapper'>
+          <img src={lightBulb} alt='Light Bulb' className='form-bulb__img' />
+          <img src={formCircle} alt='Form Circle' />
+        </div>
+        <div className='welcome-text__wrapper'>
+          <span className='welcome__text'>Welcome!</span>
+          <span className='welcome-msg__text'>Create your account to get started. After that, you can share books and make friends.</span>
+        </div>
+
+        <div className='form__wrapper'>
+          <span className='welcome-email__text'>Email</span>
+          <img src={inputCircle} alt='Input Circle' className='input-circle' />
+          <input className='welcome-email__input' type='email' />
+          <span className='welcome-password__text'>Password</span>
+          <input className='welcome-password__input' type='password' />
+          <span className='welcome-confirm-password__text'>Type Password Again</span>
+          <input className='welcome-confirm-password__input' type='password' />
+          <button className='welcome-btn__signup'>Signup</button>
+          <span className='redirect-login__text'>Already have an account? <a className='link__text' href='#/login'>Login</a></span>
+        </div>
       </div>
     </div>
   </div>
