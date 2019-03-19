e2252e6bd2ae7cf2676e36b96e0d234bb3870dfc
rkterungwa16 <terungwakombol@gmail.com>
2019-03-19 06:51:52 +0100

    Implement review page

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import formCircle from '../assets/review-middle-ellipse.svg'
 import lightBulb from '../assets/review-star.svg'
+import rightArrow from '../assets/review-arrow.svg'
+import lowerCircle from '../assets/review-lower-ellipse.svg'
 
 const Review = () => (
   <div className='welcome__container'>
@@ -11,7 +13,15 @@ const Review = () => (
         <img src={formCircle} alt='Form Circle' className='review-mid__circle' />
         <span className='review__text'>Review Them</span>
         <span className='review-msg__text'>Create your account to get started. After that, you can share books and make friends.</span>
-        <button className='review-btn'>Signup</button>
+        <div className='dot__wrapper'>
+          <span className='dot'></span>
+          <span className='dot active'></span>
+          <span className='dot'></span>
+        </div>
+        <button className='review-btn'>
+          <img src={rightArrow} alt='Right Arrow' />
+        </button>
+        <img src={lowerCircle} alt='Lower Circle' className='lowerCircle' />
         <span className='review__link'><a href='#/skip-intro' className='link__text'>Skip Intro</a></span>
       </div>
     </div>
